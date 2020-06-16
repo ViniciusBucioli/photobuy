3219f69c17a26d06e516b270122c4420b50c7a1d
yugo-harago <yugiharagg.yh@gmail.com>
2020-06-15 21:04:47 -0300

    Trying using multiparser

--- application/controller/header.php
+++ application/controller/header.php
@@ -1,20 +1,32 @@
 <?php
+
+    require_once '../../MultipartParser/ParserSelector.php';
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
     // header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Lang");
 
-    ini_set("allow_url_fopen", true);
-    global $_DELETE;
-    $_DELETE = array();
-    global $_PUT;
-    $_PUT = array();
+    // ini_set("allow_url_fopen", true);
+    // global $_DELETE;
+    // $_DELETE = array();
+    // global $_PUT;
+    // $_PUT = array();
+    // use MultipartParser;
 
-    if (!strcasecmp($_SERVER['REQUEST_METHOD'], 'DELETE')) {
-        parse_str(file_get_contents('php://input'), $_DELETE);
-    }
-    if (!strcasecmp($_SERVER['REQUEST_METHOD'], 'PUT')) {
-        parse_str(file_get_contents('php://input'), $_PUT);
-    }
+    // $parserSelector = new ParserSelector();
+    // $contentType = 'multipart/form-data;';
+
+    // parse_str(file_get_contents('php://input'), $content);
+
+    // if (!strcasecmp($_SERVER['REQUEST_METHOD'], 'DELETE')) {
+    //     $parser = $parserSelector->getParserForContentType($contentType);
+    //     $_DELETE = $parser->parse($content);
+    // }
+    // if (!strcasecmp($_SERVER['REQUEST_METHOD'], 'PUT')) {
+    //     $parser = $parserSelector->getParserForContentType($contentType);
+    //     $_PUT = $parser->parse($content);
+    // }
+    // print_r($_PUT);
+    // print_r($_DELETE);
 ?>
---
+++ multipart-master/README.md
@@ -0,0 +1,31 @@
+# h4cc/multipart
+[![Build Status](https://travis-ci.org/h4cc/multipart.svg?branch=master)](https://travis-ci.org/h4cc/multipart)
+
+A PHP library for parsing (and generating) [RFC1341 Multipart](http://www.w3.org/Protocols/rfc1341/7_2_Multipart.html).
+
+Finally a Multipart parser library for PHP!
+
+## Usage
+
+```php
+$content = '--------------------------eb2d2b296b73a011' . "\r\n"
+    . 'Content-Disposition: form-data; name="foo"' . "\r\n"
+    . 'Content-Type: text/plain' . "\r\n\r\n"
+    . 'bar' . "\r\n"
+    . 'buz' . "\r\n"
+    . '--------------------------eb2d2b296b73a011--' . "\r\n";
+$contentType = 'multipart/form-data; boundary=a3d8fcba372c456a';
+
+$parserSelector = new ParserSelector();
+$parser = $parserSelector->getParserForContentType($contentType);
+$multipart = $parser->parse($content);
+
+var_dump($multipart);
+```
+
+## Current Status
+
+I needed a multipart parser, so a wrote one.
+If anybody would like to write a Generator, do so :)
+
+
